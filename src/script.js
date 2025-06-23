2c5f96d0c5160490e4e3af84590fb815346febbb
tosha504 <tosha.rovniy@gmail.com>
2025-06-23 21:36:17 +0200

    finish footer section

--- src/script.js
+++ src/script.js
@@ -1,10 +1,12 @@
 window.addEventListener("load", (event) => {
   const burger = document.getElementById("burger"),
-    menu = document.getElementById("menu");
+    menu = document.getElementById("menu"),
+    body = document.querySelector("body");
 
   burger.addEventListener("click", () => {
     burger.classList.toggle("open");
     menu.classList.toggle("active");
+    body.classList.toggle("overflow-hidden");
   });
   console.log("page is fully loaded");
 });
