84c8b79bd880ddfa3a04187f691c02925c0abdfc
tosha504 <tosha.rovniy@gmail.com>
2025-06-24 22:24:20 +0200

    create a partners section

--- src/script.js
+++ src/script.js
@@ -8,5 +8,29 @@ window.addEventListener("load", (event) => {
     menu.classList.toggle("active");
     body.classList.toggle("overflow-hidden");
   });
+
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 3, // 👈 Show 3 slides at once
+    //   spaceBetween: 10,
+    breakpoints: {
+      0: {
+        slidesPerView: 3,
+      },
+
+      768: {
+        slidesPerView: 5,
+      },
+      992: {
+        slidesPerView: 7,
+      },
+    },
+    autoplay: {
+      delay: 2500,
+    },
+    freeModeMomentum: false, // prevents acceleration/deceleration
+    allowTouchMove: false,
+    simulateTouch: false,
+  });
   console.log("page is fully loaded");
 });
